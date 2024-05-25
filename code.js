0e42ef91e2ab2f0d68ebe4216d14514ec39646b1
Samuele Burattini <32274195+samubura@users.noreply.github.com>
2024-05-25 18:09:24 +0200

    Basic HTML page

--- code.js
+++ code.js
@@ -1,7 +1,46 @@
-function ledOn(){
-    UART.write("LED2.set();\n");
+let connection = undefined;
+
+// Called when we get a line of data 
+function onLine(line) {
+    try {
+        let data = JSON.parse(line);
+        console.log("Received JSON: ",data);
+        //update view
+        updateView(data);
+    } catch(e) {
+        console.log("Received: ",line);
+    }
 }
 
-function ledOff(){
-    UART.write("LED2.reset();\n");
+
+//creates the connection and register the handler for received data
+function connectDevice() {
+    Puck.connect(function(c) {
+        if (!c) {
+            alert("Couldn't connect!");
+            return;
+        }
+        connection = c;
+        let buf = "";
+        connection.on("data", function(d) {
+        //handle one "line" of data at a time
+        buf += d;
+        let i = buf.indexOf("\n");
+        while (i>=0) {
+            onLine(buf.substring(0,i));
+            buf = buf.substring(i+1);
+            i = buf.indexOf("\n");
+        }
+        });
+    });
+}
+
+function start(){
+    if(connection){
+        Puck.write("startSending()");
+    }
+}
+
+function updateView(data){
+    document.getElementById("data").innerHTML = JSON.stringify(data);
 }
